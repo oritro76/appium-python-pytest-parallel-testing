3e78b6609ead7d6f0b928ff31906827b3a4661d4
Aritra Banerjee <arira.banerjee207@gmail.com>
2021-08-16 16:12:55 +0600

    changed log level from error to info

--- conf/conf.py
+++ conf/conf.py
@@ -17,21 +17,21 @@ APP_ACTIVITY = "SplashScreenActivity"
 
 logger.remove()
 
-logger.add(sys.stderr, level="ERROR")
+logger.add(sys.stderr, level="INFO")
 
 logger.add(sink=APPIUM_SERVER_LOG_PATH,
            filter=lambda record: record["extra"]["appium_server"] == "appium_server",
-           level='ERROR',
+           level='INFO',
            mode="w")
 
 logger.add(LOGCAT_LOG_PATH,
            filter=lambda record: record["extra"]["logcat"] == "logcat",
-           level='ERROR',
+           level='INFO',
            mode="w")
 
 logger.add(TESTS_LOG_PATH,
            filter=lambda record: record["extra"]["test"] == "test",
-           level='ERROR',
+           level='INFO',
            mode="w")
 
 logger_appium_server = logger.bind(appium_server="appium_server")
